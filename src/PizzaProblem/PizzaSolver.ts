b73324873a86e9d0e7b367006635a887e89451b8
Jorge Rodriguez <jorge.rodriguez@piksel.com>
2018-02-28 12:51:40 +0100

    Added input read for pizza example

--- src/PizzaProblem/PizzaSolver.ts
+++ src/PizzaProblem/PizzaSolver.ts
@@ -1,5 +1,30 @@
 import { ProblemSolver } from "../utils/ProblenSolver";
 
 export class PizzaSolver extends ProblemSolver {
+  private pizza: Array<Array<number>> = [];
+  private r: number;
+  private c: number;
+  private l: number;
+  private h: number;
+
+  public parseInput(){
+    const parameters: string = this.fileScanner.nextLine();
+    const values: Array<string> = parameters.split(" ");
+
+    this.r = values[0];
+    this.c = values[1];
+    this.l = values[2];
+    this.h = values[3];
+
+    for (let i = 0 ; i < this.r ; ++i){
+      const line: string = this.fileScanner.nextLine();
+      const arrayOfChar = line.split("");
+      this.pizza.push(arrayOfChar);
+    }
+  }
+
+  public solve(){
+    console.log("input",this);
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
 import { argv } from "yargs";
+import { PizzaSolver} from "./PizzaProblem/PizzaSolver";
 // console.log(argv.prueba);
+
+const pizzaSolver = new PizzaSolver("./src/PizzaProblem/input.txt");
+pizzaSolver.parseInput();
+pizzaSolver.solve();
