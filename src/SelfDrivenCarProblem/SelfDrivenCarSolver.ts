9baf87baff568c7e48e7b6818240a1314ceaae2f
Jorge Rodriguez <jorge.rodriguez@piksel.com>
2018-03-01 19:28:41 +0100

    fixed typo

---
+++ src/SelfDrivenCarProblem/SelfDrivenCarSolver.ts
@@ -0,0 +1,10 @@
+import { ProblemSolver } from "../utils/ProblemSolver";
+
+export class SelfDrivedCarSolver extends ProblemSolver {
+    parseInput() {
+        throw new Error("Method not implemented.");
+    }
+    solve() {
+        throw new Error("Method not implemented.");
+    }
+}
