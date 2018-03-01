ad4d27d54b42783c80ba435cef4f8e08dc20a015
sifaw <metaforaideas@gmail.com>
2018-03-01 20:42:56 +0100

    tabs with 2 spaces

--- src/SelfDrivenCarProblem/SelfDrivenCarSolver.ts
+++ src/SelfDrivenCarProblem/SelfDrivenCarSolver.ts
@@ -4,37 +4,37 @@ import { Car } from "./model/Car";
 
 export class SelfDrivenCarSolver extends ProblemSolver {
 
-    private R: number;
-    private C: number;
-    private F: number;
-    private N: number;
-    private B: number;
-    private T: number;
+  private R: number;
+  private C: number;
+  private F: number;
+  private N: number;
+  private B: number;
+  private T: number;
 
-    private rides: Ride[] = [];
-    private cars: Car[] = [];
+  private rides: Ride[] = [];
+  private cars: Car[] = [];
 
-    parseInput() {
-        [this.R, this.C, this.F, this.N, this.B, this.T] = this.fileScanner.nextLine()
-            .split(" ").map(l => +l);
+  parseInput() {
+    [this.R, this.C, this.F, this.N, this.B, this.T] = this.fileScanner.nextLine()
+      .split(" ").map(l => +l);
 
-        for (let i = 0; i < this.F; i++) {
-            this.cars.push(new Car(i));
-        }
-
-        for (let i = 0; i < this.N; i++) {
-            const [a, b, x, y, s, f] = this.fileScanner.nextLine().split(" ").map(l => +l);
-            this.rides.push(new Ride(a, b, x, y, s, f, this.B, i));
-        }
+    for (let i = 0; i < this.F; i++) {
+      this.cars.push(new Car(i));
     }
 
-    solve() {
-        while (this.cars[0].currentStep < this.T && this.rides.length > 0) {
-            this.cars[0].getBestRide(this.rides);
-            this.cars = this.cars.sort((a, b) => {
-                return a.currentStep - b.currentStep;
-            });
-        }
+    for (let i = 0; i < this.N; i++) {
+      const [a, b, x, y, s, f] = this.fileScanner.nextLine().split(" ").map(l => +l);
+      this.rides.push(new Ride(a, b, x, y, s, f, this.B, i));
     }
+  }
+
+  solve() {
+    while (this.cars[0].currentStep < this.T && this.rides.length > 0) {
+      this.cars[0].getBestRide(this.rides);
+      this.cars = this.cars.sort((a, b) => {
+        return a.currentStep - b.currentStep;
+      });
+    }
+  }
 
 }
