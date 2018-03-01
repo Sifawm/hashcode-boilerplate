906d00667fd24e5e8ad2f70d7a77d62832feb7b7
sifaw <metaforaideas@gmail.com>
2018-03-01 20:35:59 +0100

    parse input

--- src/SelfDrivenCarProblem/SelfDrivenCarSolver.ts
+++ src/SelfDrivenCarProblem/SelfDrivenCarSolver.ts
@@ -1,10 +1,40 @@
 import { ProblemSolver } from "../utils/ProblemSolver";
+import { Ride } from "./model/Ride";
+import { Car } from "./model/Car";
 
-export class SelfDrivedCarSolver extends ProblemSolver {
+export class SelfDrivenCarSolver extends ProblemSolver {
+
+    private R: number;
+    private C: number;
+    private F: number;
+    private N: number;
+    private B: number;
+    private T: number;
+
+    private rides: Ride[] = [];
+    private cars: Car[] = [];
+
     parseInput() {
-        throw new Error("Method not implemented.");
+        [this.R, this.C, this.F, this.N, this.B, this.T] = this.fileScanner.nextLine()
+            .split(" ").map(l => +l);
+
+        for (let i = 0; i < this.F; i++) {
+            this.cars.push(new Car(i));
+        }
+
+        for (let i = 0; i < this.N; i++) {
+            const [a, b, x, y, s, f] = this.fileScanner.nextLine().split(" ").map(l => +l);
+            this.rides.push(new Ride(a, b, x, y, s, f, this.B, i));
+        }
     }
+
     solve() {
-        throw new Error("Method not implemented.");
+        while (this.cars[0].currentStep < this.T && this.rides.length > 0) {
+            this.cars[0].getBestRide(this.rides);
+            this.cars = this.cars.sort((a, b) => {
+                return a.currentStep - b.currentStep;
+            });
+        }
     }
+
 }
